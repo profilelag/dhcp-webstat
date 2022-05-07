7a3692a2472954fdbba5976ac936875f8c040d3d
Nicholas Keller <nkeller@profilelag.org>
2022-05-07 19:51:45 -0400

    lol

--- server.js
+++ server.js
@@ -1,44 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const https = require('https');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const { spawnSync } = require('child_process');
-const jsonParser = bodyParser.json()
+const { createServer } = require("https");
+const { json } = require("body-parser");
+const { readFileSync } = require("fs");
+const { spawnSync } = require("child_process");
 
-using_https = true
+const using_https = true,
+	http_port = 8080;
 
-const args = process.argv.slice(-2,process.argv.length)
+const args = process.argv.slice(-2, process.argv.length);
 
-const sshcmd = (username, pass)=>{return spawnSync("sshpass", ["-e", "ssh", "-p" + args[0], username + "@" + args[1], "/sbin/dhcp-lease-list"], { env: {SSHPASS:pass}});}
+const sshcmd = (username, pass) => spawnSync("sshpass",["-e","ssh","-p" + args[0],username + "@" + args[1],"/sbin/dhcp-lease-list"],{ env: { SSHPASS: pass } });
 
-const options = {}
-app.get('/', function(req, res) {
-	res.write(fs.readFileSync(path.join(__dirname, "./index.html")));
-	res.end();
-});
+app.use(express.static(`${__dirname}/public`));
 
-app.get('/frapp.js', function(req, res) {
-	res.write(fs.readFileSync(path.join(__dirname, "./frapp.js")));
-	res.end();
-});
-
-
-app.post('/login', jsonParser, (req, res) => {
+app.post("/login", json(), async(req, res) => {
 	const output = sshcmd(req.body.user, req.body.pass);
-	if(output.status == 0){
-		res.send({status:output.status,response:output.stdout.toString()});
-	} else {
-		res.send({status:output.status,response:"Aomething went wrong."});
-	}
-	res.end();
+	if (output.status == 0) res.end(JSON.stringify({ status: output.status, response: output.stdout.toString() }));
+	else res.end(JSON.stringify({ status: output.status, response: "Something went wrong." }));
 });
-if(using_https){
-const options = {
-  key: fs.readFileSync('keys/key.pem'),
-  cert: fs.readFileSync('keys/cert.pem')
-};
-https.createServer(options, app).listen(8080);
-} else
-app.listen(8080);
+
+if (using_https) {
+	createServer({
+		key: readFileSync("keys/key.pem"),
+		cert: readFileSync("keys/cert.pem"),
+	}, app).listen(443);
+} else app.listen(http_port);
